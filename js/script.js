213fa423f117d13a4d1cdce63782176e317ea2d7
Scott Sanford <scottsanford@Scotts-MacBook-Pro.local>
2015-01-16 17:33:50 -0600

    Add gender feature to app

--- js/script.js
+++ js/script.js
@@ -1,34 +1,41 @@
 angular.module("myApp",[])
+
+	.constant('VERSION', 2)
+	.run(function(VERSION, $rootScope){
+		$rootScope = VERSION;
+	})
+
+
 	.controller("madLibCtrl", function($scope){
+
 		$scope.words = {
-			timeofDay : '', 
-			adjectiveOne : '',
-			nounOne : '',
-			place : '', 
-			adverbOne : '', 
-			ED_Verb : '', 
-			nounTwo : '', 
-			adjectiveTwo : '', 
-			color : '', 
-			nounThree : '', 
-			adjectiveThree : '', 
-			article_of_clothing_plural : '', 
-			bodyPart : '', 
-			adjectiveFour : '', 
-			adverbTwo : '', 
-			liquid : '', 
-			bodyPartTwo : '', 
-			adjectiveFive : '', 
-			ingVerb : '', 
-			adjectiveSix : '', 
-			liquidTwo : '', 
-			nounPlural : '', 
-			food : '', 
-			colorTwo : '', 
-			animal : '', 
-			verb : '', 
-			adverbThree : '', 
-			yourname : ''
-		}; 
+			name: '', 
+			jobTitle: '', 
+			tediousTask: '', 
+			dirtyTask: '', 
+			celebrity: '', 
+			adjective: '', 
+			hugeNum: ''
+		};
+
+		$scope.gender = new Gender(true);
 
 	});
+
+function Gender(isFemale) {
+    	this.isFemale = isFemale;
+
+    	this.himHer = function () {
+        return this.isFemale ? "her" : "him";
+    	}
+
+   	    this.heShe = function () {
+        return this.isFemale ? "she" : "he";
+    	}
+
+    	this.hisHer = function () {
+        return this.isFemale ? "her" : "his";
+    	}
+
+}
+	

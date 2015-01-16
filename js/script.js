2e7dc6e4c83c835036f4c8c6e42c18429aaeff65
Scott Sanford <scottsanford@Scotts-MacBook-Pro.local>
2015-01-16 13:33:22 -0600

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+angular.module("myApp",[])
+	.controller("madLibCtrl", function($scope){
+		$scope.words = {
+			timeofDay : '', 
+			adjectiveOne : '',
+			nounOne : '',
+			place : '', 
+			adverbOne : '', 
+			ED_Verb : '', 
+			nounTwo : '', 
+			adjectiveTwo : '', 
+			color : '', 
+			nounThree : '', 
+			adjectiveThree : '', 
+			article_of_clothing_plural : '', 
+			bodyPart : '', 
+			adjectiveFour : '', 
+			adverbTwo : '', 
+			liquid : '', 
+			bodyPartTwo : '', 
+			adjectiveFive : '', 
+			ingVerb : '', 
+			adjectiveSix : '', 
+			liquidTwo : '', 
+			nounPlural : '', 
+			food : '', 
+			colorTwo : '', 
+			animal : '', 
+			verb : '', 
+			adverbThree : '', 
+			yourname : ''
+		}; 
+
+	});

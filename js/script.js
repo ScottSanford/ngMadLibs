72ad0af03be850c40f0368f77cf54611b10fc506
Scott Sanford <scottsanford@Scotts-MacBook-Pro.local>
2015-01-20 17:47:37 -0600

    Add form validation and new views

--- js/script.js
+++ js/script.js
@@ -1,10 +1,11 @@
-angular.module("myApp",[])
+angular.module("myApp",[
+		'ngMessages'
+	])
 
 	.constant('VERSION', 2)
 	.run(function(VERSION, $rootScope){
 		$rootScope = VERSION;
 	})
-
 
 	.controller("madLibCtrl", function($scope){
 
@@ -18,24 +19,45 @@ angular.module("myApp",[])
 			hugeNum: ''
 		};
 
-		$scope.gender = new Gender(true);
+		$scope.genderSelect = [
+			{show: 'Pick the gender of your name...', bool: null},
+			{show: 'Male', bool: false},
+			{show: 'Female', bool: true}
+			];
+		
+		$scope.current = $scope.genderSelect[0];
+
+  		$scope.isFemale = true;
+
+  		$scope.heShe = function(bool) {
+            if ($scope.current.bool == true) {
+                return "she";
+            } else {
+            	return "he";
+            }
+        };
+
+        $scope.himHer = function(bool) {
+            if ($scope.current.bool == true) {
+                return "her";
+            } else {
+            	return "him";
+            }
+        };
+
+		$scope.generateMadLib = false;
+
+		$scope.submit = function(){
+        	$scope.generateMadLib = true;
+    	}
+
+    	$scope.reset = function(){
+	        $scope.words = {};
+	        $scope.generateMadLib = false;
+	        $scope.current = $scope.genderSelect[0];
+	        $scope.madLibForm.$setPristine();
+  		}
 
 	});
 
-function Gender(isFemale) {
-    	this.isFemale = isFemale;
-
-    	this.himHer = function () {
-        return this.isFemale ? "her" : "him";
-    	}
-
-   	    this.heShe = function () {
-        return this.isFemale ? "she" : "he";
-    	}
-
-    	this.hisHer = function () {
-        return this.isFemale ? "her" : "his";
-    	}
-
-}
 	

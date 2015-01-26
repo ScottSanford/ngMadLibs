0c1d1f75499d7f61d69aaa0904342650411dd2ed
Scott Sanford <scottsanford@Scotts-MacBook-Pro.local>
2015-01-26 15:20:17 -0600

    Add animation

--- js/script.js
+++ js/script.js
@@ -1,13 +1,23 @@
 angular.module("myApp",[
-		'ngMessages'
+		'ngMessages', 
+		'ngAnimate'
 	])
 
-	.constant('VERSION', 2)
+	.constant('VERSION', 3)
 	.run(function(VERSION, $rootScope){
 		$rootScope = VERSION;
 	})
 
 	.controller("madLibCtrl", function($scope){
+
+		$scope.initForm = function() {
+
+	        $scope.words = {};
+	        $scope.generateMadLib = false;
+	        $scope.current = $scope.genderSelect[0];
+	        $scope.madLibForm.$setPristine();
+
+    	}
 
 		$scope.words = {
 			name: '', 
@@ -15,9 +25,24 @@ angular.module("myApp",[
 			tediousTask: '', 
 			dirtyTask: '', 
 			celebrity: '', 
+			uselessSkill: '',
 			adjective: '', 
 			hugeNum: ''
 		};
+
+		$scope.sampleData = function(event) {
+			$scope.words = {
+				name: 'Kelly', 
+				jobTitle: 'Senior Software Engineer', 
+				tediousTask: 'mowing the lawn', 
+				dirtyTask: 'changing dirty diapers', 
+				celebrity: 'Gregory McClendon', 
+				uselessSkill: 'Ping Pong',
+				adjective: 'sexy', 
+				hugeNum: 100000
+			};
+			$scope.genderSelect[2];
+		}
 
 		$scope.genderSelect = [
 			{show: 'Pick the gender of your name...', bool: null},
@@ -26,8 +51,6 @@ angular.module("myApp",[
 			];
 		
 		$scope.current = $scope.genderSelect[0];
-
-  		$scope.isFemale = true;
 
   		$scope.heShe = function(bool) {
             if ($scope.current.bool == true) {
@@ -45,17 +68,24 @@ angular.module("myApp",[
             }
         };
 
+        $scope.hisHer = function(bool) {
+            if ($scope.current.bool == true) {
+                return "her";
+            } else {
+            	return "his";
+            }
+        };
+
 		$scope.generateMadLib = false;
 
 		$scope.submit = function(){
-        	$scope.generateMadLib = true;
+			if ($scope.madLibForm.$valid) {
+        		$scope.generateMadLib = true;
+			}
     	}
 
     	$scope.reset = function(){
-	        $scope.words = {};
-	        $scope.generateMadLib = false;
-	        $scope.current = $scope.genderSelect[0];
-	        $scope.madLibForm.$setPristine();
+    		$scope.initForm();
   		}
 
 	});
